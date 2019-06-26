017b8e486689df03157da0e6e17070cc26f47ed1
feng <1098865109@qq.com>
2019-06-26 11:52:31 +0800

    商品管理

--- src/view/goods/Goods.vue
+++ src/view/goods/Goods.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="goods-page">
-        <el-button type="primary" @click="add">更新num</el-button>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item>首页</el-breadcrumb-item>
+            <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+            <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <el-divider></el-divider>
     </div>
 </template>
 
